e0e3ff06e3d5fb7976ac232efe65f5cb48ab6f24
# <willamis.silva@vectracs.com.br>
2020-02-25 22:15:47 -0300

    atualizando projeto

--- app.js
+++ app.js
@@ -8,23 +8,16 @@ const auth = require('./routes/auth');
 
 const app = express();
 
-
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.use(cors());
 
-mongoose.connect('mongodb+srv://will:Aoa67m9A809NiZxD@atlabox-nkckf.mongodb.net/acervo?retryWrites=true&w=majority', {useNewUrlParser: true,
-useUnifiedTopology: true});
-
 app.use('/auth', auth)
 app.use('/api', api);
-
 
 app.use((req, res, next)=>{
     res.status(404).send('Not found')
 });
 
-
-app.listen(3000);
+module.exports = app;
